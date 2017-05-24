ac0f2db8d68ffb2546054b064033d0572c29108d
adrilord <adrilord@gmail.com>
2017-05-24 16:36:08 +0200

    Gestion de l'affichage des menus

--- test.c
+++ test.c
@@ -28,15 +28,16 @@ typedef struct Showdatetime {
 	char hundredths[3];
 }Showdatetime;
 
-Showdatetime datetime2Showdatetime(Datetime datetime)
+Showdatetime datetime2Showdatetime(Datetime* datetime)
 {
 	Showdatetime showdatetime;
-	sprintf(showdatetime.year, "%d", datetime.year);
-	sprintf(showdatetime.month, "%d", datetime.month);
-	sprintf(showdatetime.day, "%d", datetime.day);
-	sprintf(showdatetime.minute, "%d", datetime.minute);
-	sprintf(showdatetime.second, "%d", datetime.second);
-	sprintf(showdatetime.hundredths, "%d", datetime.hundredths);
+	snprintf(showdatetime.year, sizeof showdatetime.year, "%d", datetime->year);
+	snprintf(showdatetime.month, sizeof showdatetime.month, "%d", datetime->month);
+	snprintf(showdatetime.day, sizeof showdatetime.day, "%d", datetime->day);
+	snprintf(showdatetime.hour, sizeof showdatetime.hour, "%d", datetime->hour);
+	snprintf(showdatetime.minute, sizeof showdatetime.minute, "%d", datetime->minute);
+	snprintf(showdatetime.second, sizeof showdatetime.second, "%d", datetime->second);
+	snprintf(showdatetime.hundredths, sizeof showdatetime.hundredths, "%d", datetime->hundredths);
 	return showdatetime;
 }
 
@@ -55,6 +56,43 @@ typedef struct Model {
 	short satellites;
 	long hdop;
 }Model;
+
+typedef struct ShowModel {
+	//local prog data
+	char autonomy[3];
+	Showdatetime localShowDateTime;
+	char durationParcours[7]; // HH:MM:SS
+	char durationInterval[7]; //MM:SS:hShS
+	//GPS data
+	char latitude[7], longitude[7], altitude[7]; // xx.xxx
+	char course[7];
+	char speed[7];
+	Showdatetime GPSShowDateTime;
+	char satellites[3];
+	char hdop[7];
+}ShowModel;
+
+ShowModel modelToShowModel(Model* model)
+{
+	ShowModel showModel;
+	snprintf(showModel.autonomy, sizeof showModel.autonomy, "%d", model->autonomy);
+	showModel.localShowDateTime = datetime2Showdatetime(&model->localDateTime);
+	snprintf(showModel.durationParcours, sizeof showModel.durationParcours, "%d:%d:%d", (model->durationParcours/3600),
+		(model->durationParcours%3600)/60,(model->durationParcours%3600)%60);
+	snprintf(showModel.durationInterval, sizeof showModel.durationInterval,"%d:%d:%d", (model->durationParcours/6000),
+		(model->durationParcours%6000)/100,(model->durationParcours%6000)%100);
+	snprintf(showModel.latitude, sizeof showModel.latitude, "%f", model->latitude);
+    snprintf(showModel.longitude, sizeof showModel.longitude, "%f", model->longitude);
+    snprintf(showModel.altitude, sizeof showModel.altitude, "%f", model->altitude);
+    snprintf(showModel.course, sizeof showModel.course, "%f", model->course);
+    snprintf(showModel.speed, sizeof showModel.speed, "%f", model->speed);
+    showModel.GPSShowDateTime = datetime2Showdatetime(&model->GPSDateTime);
+    snprintf(showModel.longitude, sizeof showModel.longitude, "%f", model->longitude);
+    snprintf(showModel.satellites, sizeof showModel.satellites, "%d", model->satellites);
+    snprintf(showModel.hdop, sizeof showModel.hdop, "%ld", model->hdop);
+    
+    return showModel;
+}
 
 //~ updateModelGPSdata(Model model, TinyGPS gps)
 //~ {
@@ -86,54 +124,118 @@ void showMenu(Menu menu)
 		printf("%c",menu.cases[i]);
 	}
 	printf("\n");
-	for(int i=NUMBER_OF_CASES/2+1; i<NUMBER_OF_CASES; i++) {
+	for(int i=NUMBER_OF_CASES/2; i<NUMBER_OF_CASES; i++) {
 		printf("%c",menu.cases[i]);
 	}
 	printf("\n");
 }
 
-void generateMenu(Menu* menu, Model* model, MENU_TYPE menu_type)
+void generateMenu(Menu* menu, ShowModel* showModel, MENU_TYPE menu_type)
 {
-	Showdatetime localShowdatetime = datetime2Showdatetime(model->localDateTime);
-	Showdatetime GPSShowdatetime = datetime2Showdatetime(model->GPSDateTime);
-	char* result;
+	char result[17]="";
 	switch(menu_type) {
 		case ACCUEIL :
-			//~ strcat(result,localShowdatetime.hour);
-			//~ strcpy(menu.cases,);
+			strcat(result,showModel->localShowDateTime.hour);
+			strcat(result,":");
+			strcat(result,showModel->localShowDateTime.minute);
+			strcat(result,":");
+			strcat(result,showModel->localShowDateTime.second);
+			strcat(result,"BAT");
+			strcat(result,":");
+			strcat(result,showModel->autonomy);
+			strcat(result,"%");
+			strcpy(menu->cases,result);
 		break;
-		case DATE :
-		break;
-		PARCOURS :
-		break; 
-		INTERVAL :
-		break;
-		COORDS1 :
-		break; 
-		COORDS2 :
-		break;
-		COORDS3 :
-		break;
-		COORDS4 :
-		break;
-		COORDS5 :
-		break;
-		ENR1: 
-		break;
-		ENR2:
-		break;
+		//~ case DATE :
+		//~ break;
+		//~ PARCOURS :
+		//~ break; 
+		//~ INTERVAL :
+		//~ break;
+		//~ COORDS1 :
+		//~ break; 
+		//~ COORDS2 :
+		//~ break;
+		//~ COORDS3 :
+		//~ break;
+		//~ COORDS4 :
+		//~ break;
+		//~ COORDS5 :
+		//~ break;
+		//~ ENR1: 
+		//~ break;
+		//~ ENR2:
+		//~ break;
 		default :
 		break;
 	}
 }
 
+void initModel(Model* model)
+{
+	model->autonomy=100;
+	model->localDateTime.year=2017;
+	model->localDateTime.month=1;
+	model->localDateTime.day=1;
+	model->localDateTime.hour=10;
+	model->localDateTime.minute=10;
+	model->localDateTime.second=10;
+	model->localDateTime.hundredths=0;
+	model->durationParcours=100; model->durationInterval=10;
+	model->latitude=50; model->longitude=40; model->altitude=30;
+	model->course=10;
+	model->speed=20;
+	model->GPSDateTime.year=2017;
+	model->GPSDateTime.month=1;
+	model->GPSDateTime.day=1;
+	model->GPSDateTime.hour=10;
+	model->GPSDateTime.minute=10;
+	model->GPSDateTime.second=10;
+	model->GPSDateTime.hundredths=0;
+	model->satellites=0;
+	model->hdop=0;
+}
+
+void initShowDateTime(Showdatetime* showdatetime)
+{
+	strcpy(showdatetime->year,"");
+	strcpy(showdatetime->month,"");
+	strcpy(showdatetime->day,"");
+	strcpy(showdatetime->hour,"");
+	strcpy(showdatetime->minute,"");
+	strcpy(showdatetime->second,"");
+	strcpy(showdatetime->hundredths,"");
+}
+
+void initShowModel(ShowModel* showModel)
+{
+	//local prog data
+	strcpy(showModel->autonomy,"");
+	initShowDateTime(&showModel->localShowDateTime);
+	strcpy(showModel->durationParcours,"");
+	strcpy(showModel->durationInterval,"");
+	//GPS data
+	strcpy(showModel->latitude,"");strcpy(showModel->longitude,"");
+	strcpy(showModel->altitude,"");strcpy(showModel->course,"");
+	strcpy(showModel->speed,"");
+	initShowDateTime(&showModel->GPSShowDateTime);
+	strcpy(showModel->satellites,"");
+	strcpy(showModel->hdop,"");
+}
+
 int main()
 {
-	//~ Menu accueil;
-	//~ strcpy(accueil.cases, "BonjourLOL10rorr");
-	//~ showMenu(accueil);
-	char* lol1 = "LOL?   ";
-	char* lol2 = "WTF?";
-	//~ strcat(lol1,lol2);
-	printf(lol1);
+	Model momo;
+	ShowModel showMomo;
+	Menu accueil;
+	initModel(&momo);
+	initShowModel(&showMomo);
+	showMomo = modelToShowModel(&momo);
+	generateMenu(&accueil, &showMomo, ACCUEIL);
+	showMenu(accueil);
+	
+	//~ char buffer[6];
+	//~ float myFloat=4.00179115;
+	//~ snprintf(buffer, sizeof buffer, "%f", myFloat);
+	//~ printf(buffer);
 }
